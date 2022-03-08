e5c61f7f9b91555c3b12d7b61cea84872b04a252
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-03-08 20:46:02 +0200

    Aprendi a executar eventos antes de processar as actions dos botões.

--- src/main/java/com/eacuamba/faces/bean/RegistoLancamentoBean.java
+++ src/main/java/com/eacuamba/faces/bean/RegistoLancamentoBean.java
@@ -11,6 +11,8 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ActionEvent;
+import jakarta.faces.event.ValueChangeEvent;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -30,6 +32,22 @@ public class RegistoLancamentoBean implements Serializable {
 
     public void prepararCadastro(){
         this.pessoaList = this.pessoaService.buscarTodasPessoas();
+    }
+
+    //Executado quando uma acção é executada, acçao dos buttons.
+    public void mostraLogSalvamento(ActionEvent actionEvent){
+        System.out.println("Registando... " + actionEvent.getSource());
+        System.out.println("Registando... " + actionEvent.getComponent());
+        System.out.println("Registando2... " + actionEvent);
+
+    }
+
+    //Só é accionado quando o valor doque estamos ouvindo muda, caso não mude não executa.
+    public void descricaoModificada(ValueChangeEvent valueChangeEvent){
+        System.out.println("Mudanca OLD: " + valueChangeEvent.getOldValue());
+        System.out.println("Mudanca NEW: " + valueChangeEvent.getNewValue());
+        //Na primeira vezz que executamos estamos forçando uma renderizacaoe ele não processa o salvamento se os dados do formulario não foram alterados ele não vai passar mais por aqui, ja que os dados são iguais.
+        FacesContext.getCurrentInstance().renderResponse();
     }
 
     public void salvar(){
