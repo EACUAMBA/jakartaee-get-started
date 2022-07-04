a20f4a60931e89c197284a598f18303faff1ded3
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-04 19:01:11 +0200

    Aprendi a trabalhar com ajax, a alterar um valor de uma label sem renderizar a página.

---
+++ src/main/java/com/eacuamba/studying/capitulo_13/_13_2_rederizacao_parcial/MeuBean.java
@@ -0,0 +1,18 @@
+package com.eacuamba.studying.capitulo_13._13_2_rederizacao_parcial;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named //Defini o nome que será usado para termos a instância deste bean, neste caso será meuBean.
+@RequestScoped //Será criada uma instância deste bean para cada request que o utilizador fizer ao nosso servidor, se tivermos milhares de utilizadores, cada um que fizer o request terá uma instância deste bean disponível.
+public class MeuBean {
+    private String nome;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
