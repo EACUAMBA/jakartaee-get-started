3e38611570da88e940a227d5a597b4ad65722b24
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-07-07 00:45:49 +0200

    Adicionei mais componentes JSF no projecto.

--- src/main/java/com/eacuamba/project/domain/model/enumeration/TipoLancamento.java
+++ src/main/java/com/eacuamba/project/domain/model/enumeration/TipoLancamento.java
@@ -1,5 +1,14 @@
 package com.eacuamba.project.domain.model.enumeration;
 
+import lombok.Getter;
+
+@Getter
 public enum TipoLancamento {
-    RECEITA, DESPESA
+    RECEITA("Receita"), DESPESA("Despesa");
+
+    TipoLancamento(String designacao){
+        this.designacao = designacao;
+    }
+
+    private String designacao;
 }
